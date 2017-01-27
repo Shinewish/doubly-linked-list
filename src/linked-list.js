0c74e82d4f04e14870c374b92c457b58c8ba8b1d
AlexTheSunbeam <q@q.q>
2017-01-27 12:49:29 +0300

    first try. All methods added

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,27 +1,159 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
+    constructor() {
+    	this.length = 0;
+    	this.head = null;
+    	this.tail = null;
+    }
 
-    append(data) {}
+    append(data) {
+    	var node = new Node(data);
 
-    head() {}
+    	if (this.length) {
+    		this.tail.next = node;
+    		node.prev = this.tail;
+    		this.tail = node;
+    	} else {
+    		this.head = node;
+    		this.tail = node;
+    	}
 
-    tail() {}
+    	this.length++;
+    }
 
-    at(index) {}
+    head() {
+    	return this.head;
+    	//this.head should exist
+    }
 
-    insertAt(index, data) {}
+    tail() {
+    	return this.tail;
+    	//this.tail should exist
+    }
 
-    isEmpty() {}
+    at(index) {
+    	var currentNode = this.head,
+    	length = this.length,
+    	count = 1;
+    	//index should be valid
 
-    clear() {}
+    	while (count < index) {
+    		currentNode = currentNode.next;
+    		count++;
+    	}
 
-    deleteAt(index) {}
+    	return currentNode;
+    }
 
-    reverse() {}
+    insertAt(index, data) {
+    	var node = new Node(data),
+    		currentNode = this.at(index);
 
-    indexOf(data) {}
+    	this.at(index - 1).next = node;
+    	node.prev = this.at(index - 1);
+    	node.next = currentNode;
+    	currentNode.prev = node;
+
+
+
+    	/*if (this.length) {
+    		this.tail.next = node;
+    		node.prev = this.tail;
+    		this.tail = node;
+    	} else {
+    		this.head = node;
+    		this.tail = node;
+    	} */
+
+    	this.length++;
+    }
+
+    isEmpty() {
+    	return !this.length; // let's try it
+    }
+
+    clear() {
+    	this.length = 0;
+    	this.head = null;
+    	this.tail = null;
+    }
+
+    deleteAt(index) {    	
+		var currentNode = this.head,
+        length = this.length,
+        count = 1,
+        DelNodePrev = null,
+        DelNode = null,
+        DelNodeNext = null;
+ 
+    // first node
+    if (index === 1) {
+        this.head = currentNode.next;
+         // second node exists
+        if (!this.head) {
+            this.head.prev = null;
+        // no second node
+        } else {
+            this.tail = null;
+        }
+
+    } else if (index === this._length) {
+
+    	// last node
+    	this.tail = this.tail.previous;
+        this.tail.next = null;
+
+	} else {
+
+        // middle node
+        while (count < index) {
+        	currentNode = currentNode.next;
+        	count++;
+        }
+
+        DelNodePrev = currentNode.prev;
+        DelNode = currentNode;
+        DelNodeNext = currentNode.next;
+        DelNodePrev.next = DelNodeNext;
+        DelNodeNext.prev = DelNodePrev;
+        DelNode = null;
+    }
+
+    	this._length--;
+    }
+
+    reverse() {
+    	var secondList = new LinkedList;
+    		currentNode = this.tail,
+        	count = this.length;
+
+    	while (count > 0) {
+        	secondList.append(currentNode.data);
+        	currentNode = currentNode.prev;
+        	count--;
+        }
+
+        this = secondList;
+    }
+
+    indexOf(data) {
+    	var currentNode = this.head,
+        	length = this.length,
+        	index = -1,
+    		count = 1;
+
+    	
+    	while (count <= this.length) {
+        	if (currentNode) === data {
+        		index = count - 1;
+        	}
+        	currentNode = currentNode.next;
+        	count++;
+        }
+
+        return index;
+    }
 }
 
 module.exports = LinkedList;
